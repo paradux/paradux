c3248d6f5613ee8dfe1003eef8ebc87a15279c4a
Johannes Ernst <Johannes.Ernst@gmail.com>
2019-02-04 03:57:45 +0000

    First command, yay!

--- paradux/commands/status.py
+++ paradux/commands/status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 #
-# Dummy sub-command.
+# Print the current paradux status.
 #
 # Copyright (C) 2019 and later, Paradux project.
 # All rights reserved. License: see package.
 #
 
 def run() :
-    print( "Ho!" )
+    print( "FIXME!" )
 
 def description() :
     return """
-A dummy command saying Ho.
+Print the current paradux status
 """
